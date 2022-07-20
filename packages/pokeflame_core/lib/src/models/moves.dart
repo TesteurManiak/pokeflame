4a48143195a50a7e1c8810a2d97b93a1b0ff4d09
Guillaume Roux <rouxguillaume8@gmail.com>
2022-07-20 20:16:44 +0200

    move model

--- packages/pokeflame_core/lib/src/models/moves.dart
+++ packages/pokeflame_core/lib/src/models/moves.dart
@@ -1,7 +1,15 @@
 class GameDataMove {
   final int id;
+  final String name;
+  final String description;
+  final int? accuracy;
+  final int? power;
 
   const GameDataMove({
     required this.id,
+    required this.name,
+    required this.description,
+    this.accuracy,
+    this.power,
   });
 }
