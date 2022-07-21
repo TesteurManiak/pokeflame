467ef80301b4612b0308d9bad8f2904cf19566e8
Guillaume Roux <rouxguillaume8@gmail.com>
2022-07-21 16:15:24 +0200

    populate GameData class

--- packages/pokeflame_core/lib/src/models/game_data/game_data.dart
+++ packages/pokeflame_core/lib/src/models/game_data/game_data.dart
@@ -3,9 +3,26 @@ import 'package:pokeflame_core/pokeflame_core.dart';
 
 @immutable
 class GameData {
+  final List<GameDataAbility> abilities;
   final GameDataConstants constants;
+  final List<GameDataEggGroup> eggGroups;
+  final List<GameDataItem> items;
+  final List<GameDataMove> moves;
+  final List<GameDataNature> natures;
+  final List<GameDataPokemon> pokemon;
 
   const GameData({
+    required this.abilities,
     required this.constants,
-  });
+    required this.eggGroups,
+    required this.items,
+    required this.moves,
+    required this.natures,
+    required this.pokemon,
+  })  : assert(abilities.length > 0),
+        assert(eggGroups.length > 0),
+        assert(items.length > 0),
+        assert(moves.length > 0),
+        assert(natures.length > 0),
+        assert(pokemon.length > 0);
 }
